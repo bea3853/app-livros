dc902f74377c6eb3bef6ce567ebfd8de99154d48
Beatriz Alves <55098795+bea3853@users.noreply.github.com>
2026-05-08 20:54:06 -0300

    Reformat database.py for consistency and clarity

--- app/models/database.py
+++ app/models/database.py
@@ -6,16 +6,16 @@ from sqlalchemy.orm import sessionmaker
 SQLALCHEMY_DATABASE_URL = os.getenv('postgresql://neondb_owner:npg_CyT0qtmcYpx3@ep-falling-thunder-ap2roc8k-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require')
 
 if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
-    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://","postgresql://" , 1)
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
-engine  =  create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 class Livro(Base):
-    __tablename__ = 'livros'
-    id  = Column(Integer, primary_key=True, index=True)
-    titulo =  Column(String )
+    __tablename__ = "livros"
+    id = Column(Integer, primary_key=True, index=True)
+    titulo = Column(String)
     autor = Column(String)
 
 def init_db():
